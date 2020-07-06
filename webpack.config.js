aed03e1e71d0565cb82de13a4e62271e4407df09
Agata Kuska <kawabezcukru@onet.eu>
2020-07-06 16:51:29 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './js/app.js',
+
+    mode: 'development',
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].bundle.js',
+        publicPath: '/'
+    },
+
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        compress: true,
+        port: 3000,
+        hot: true,
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ]
+            },
+            {
+                test: /\.(jpg|png)$/,
+                use: [
+                    'file-loader',
+                ]
+            }
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({template: './index.html'}),
+        new webpack.HotModuleReplacementPlugin(),
+    ]
+};
